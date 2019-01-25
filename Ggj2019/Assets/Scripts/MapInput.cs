411ce1bf5648513211a4a1dfce3d21d3959ba636
Konstantin Rudolph <konrudolph@gmail.com>
2019-01-25 23:10:51 +0100

    [ADDED] Basic input script and player movement controllers.

---
+++ Ggj2019/Assets/Scripts/MapInput.cs
@@ -0,0 +1,47 @@
+﻿#region FileHeader
+
+// File: PlayerController.cs
+// Project: Divisor - HAW Math Games 
+// Author: Konstantin Rudolph
+// Creation Date: 22:02, 25.01.2019
+// Last Edit: 22:21, 25.01.2019 by Konstantin Rudolph
+
+#endregion
+
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class MapInput : MonoBehaviour
+{
+	public LayerMask TileLayerMask;
+
+	private Tile SelectedTile;
+
+	public PlayerMovementController PlayerMovement;
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100f, TileLayerMask))
+			{
+				var clickedTile = hit.collider.GetComponent<Tile>();
+				if (SelectedTile == null || SelectedTile != clickedTile)
+				{
+					SelectedTile = clickedTile;
+					//TODO: highlight path from WalkOnGrid
+				}
+				else // selected tile == clicked tile
+				{
+					//TODO: Confirm / trigger movement.
+				}
+			}
+		}
+		else if (Input.GetMouseButtonDown(1))
+		{
+			// Cancels path.
+			SelectedTile = null;
+		}
+	}
+}
---
+++ Ggj2019/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
