53b0a7700aceb5c86431bd264199fb5a0dcac33f
Anton Herzog <info@antonherzog.com>
2019-01-25 21:24:53 +0100

    Add Tile members.

--- Ggj2019/Assets/Scripts/Tile.cs
+++ Ggj2019/Assets/Scripts/Tile.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    public int X;
+
+    public int Y;
+
+    public DiscoveryState DiscoveryState;
+
+    public bool Walkable;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+}
+
+public enum DiscoveryState
+{
+    Hidden,
+    Discovered,
+    Visible
 }
