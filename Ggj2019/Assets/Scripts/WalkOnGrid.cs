3cf097f2adb843351e73ab491dc9b57ea9c344b8
Anton Herzog <info@antonherzog.com>
2019-01-25 21:54:12 +0100

    Add missing file

---
+++ Ggj2019/Assets/Scripts/WalkOnGrid.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkOnGrid
+{
+    public IEnumerable<Tile> GetPath(Tile[,] grid, Tile start, Tile target)
+    {
+        return new[] {start};
+    }
+}
