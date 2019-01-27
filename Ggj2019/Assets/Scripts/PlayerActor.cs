909faf580dfd50a7be03f307c2b7186ad58eb491
Konstantin Rudolph <konrudolph@gmail.com>
2019-01-27 03:09:46 +0100

    [CHANGED] AnimationController is reset properly when upgrading an item.

--- Ggj2019/Assets/Scripts/PlayerActor.cs
+++ Ggj2019/Assets/Scripts/PlayerActor.cs
@@ -57,6 +57,8 @@ public class PlayerActor : PlayerMovementController
 			{
 				var tmp = CarriedPickupableActor;
 				Destroy(tmp.gameObject);
+				AnimationController.Reset();
+				AnimationController.Idle();
 				EnteredHomeWithUpgrade.Raise();
 			}
 		}
